57161ace072592fbd6047567c98ff3f511af10a1
Jake Schaefbauer <jschaefmn@gmail.com>
2022-09-08 19:13:46 -0500

    fixed an error

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  const { name, email, message } = formState;
+  function handleChange(e) {
+    setFormState({ ...formState, name: e.target.value })
+  }
+  
+  console.log(formState);
+  
   return (
     <section>
       <h1>Contact Me</h1>
       <form id="contact-form">
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input type="text" name="name" defaultValue={name} onChange={handleChange} />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" />
+          <input type="email" name="email" defaultValue={email} />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea name="message" rows="5" defaultValue={message} />
         </div>
 
         <button type="submit">Submit</button>
